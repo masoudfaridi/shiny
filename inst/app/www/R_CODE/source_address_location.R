a4e54cb11082b8d9c29b32f2430c90a1cf551ace
masoud-faridi <masoud1faridi@gmail.com>
2024-02-28 11:59:14 +0330

    global files rebased

--- inst/app/www/R_CODE/source_address_location.R
+++ inst/app/www/R_CODE/source_address_location.R
@@ -25,16 +25,15 @@ source_global$cybersec_ir<-"https://www.cybersec.ir/"
 ## mod_module_1_1.R
 # "inst/app/www/rn_adm_unhcr_20190514/irn_admbnda_adm1_unhcr_20190514.shp"
 source_global$iran_shape_file=
-  "inst/app/www/irn_adm_unhcr_20190514/irn_admbnda_adm1_unhcr_20190514.shp"
+  "inst/app/www/files/shape_files/irn_adm_unhcr_20190514/irn_admbnda_adm1_unhcr_20190514.shp"
 
 
-##
-source_global$data_acc_test3="inst/app/www/files/acc_test3.xlsx"
+## source_loading_excel_files.R
+source_global$data_acc_test3="inst/app/www/files/excel_files/acc_test3.xlsx"
 
 
 ## app_ui.R
 
 source_global$icon_1_fanavaran_2<-"inst/app/www/images/icon_1_fanavaran_2.png"
 
-source_global$icon_1_fanavaran_2<-"inst/app/www/images/icon_1_fanavaran_2.png"
-source_global$icon_1_fanavaran_2<-"inst/app/www/images/icon_1_fanavaran_2.png"
+
